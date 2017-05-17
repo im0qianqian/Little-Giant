fa1d9b70a06a3436c893be7c9e276c9a6617c863
千千 <qian1335661317@qq.com>
2017-05-17 13:47:25 +0800

    Award

--- Classes/Character.cpp
+++ Classes/Character.cpp
@@ -39,3 +39,105 @@ void Character::die()
 {
 
 }
+
+
+Character::Attribute::Attribute() :
+	_attackDamage(0),
+	_attackRange(0),
+	_attackSpeed(0),
+	_movingSpeed(0),
+	_empiricalAcquisition(0),
+	_defensiveForce(0),
+	_restoringAbility(0),
+	_temporary(kTemporaryNone),
+	_duration(0)
+{
+
+}
+
+Character::Attribute::~Attribute()
+{
+
+}
+
+Character::Attribute* Character::Attribute::create()
+{
+	Character::Attribute *attribute = new Attribute();
+	if (!attribute)
+	{
+		CCASSERT(attribute, "Attribute not null!");
+		delete attribute;
+		attribute = NULL;
+	}
+	return attribute;
+}
+
+void Character::Attribute::addAttackDamage(float add)
+{
+	_attackDamage = max(0.0f, _attackDamage + add);
+}
+
+void Character::Attribute::addAttackRange(float add)
+{
+	_attackRange = max(0.0f, _attackRange + add);
+}
+
+void Character::Attribute::addAttackSpeed(float add)
+{
+	_attackSpeed = max(0.0f, _attackSpeed + add);
+}
+
+void Character::Attribute::addMovingSpeed(float add)
+{
+	_movingSpeed = max(0.0f, _movingSpeed + add);
+}
+
+void Character::Attribute::addEmpiricalAcquisition(float add)
+{
+	_empiricalAcquisition = max(0.0f, _empiricalAcquisition + add);
+}
+
+void Character::Attribute::addDefensiveForce(float add)
+{
+	_defensiveForce = max(0.0f, _defensiveForce + add);
+}
+
+void Character::Attribute::addRestoringAbility(float add)
+{
+	_restoringAbility = max(0.0f, _restoringAbility + add);
+}
+
+void Character::Attribute::addView()
+{
+	_temporary |= kTemporaryView;
+}
+
+void Character::Attribute::addSmall()
+{
+	_temporary |= kTemporarySmall;
+}
+
+void Character::Attribute::addMagnet()
+{
+	_temporary |= kTemporaryMagnet;
+}
+
+void Character::Attribute::delView()
+{
+	_temporary &= ~kTemporaryView;
+}
+
+void Character::Attribute::delSmall()
+{
+	_temporary &= ~kTemporarySmall;
+}
+
+void Character::Attribute::delMagnet()
+{
+	_temporary &= ~kTemporaryMagnet;
+}
+
+void Character::Attribute::setDuration(float add)
+{
+	_duration = max(0.0f, _duration + add);
+}
--- Classes/Character.h
+++ Classes/Character.h
@@ -4,7 +4,6 @@
 #define __Character_H__
 
 #include "cocos2d.h"
-#include "Attribute.h"
 #include "Weapons.h"
 
 USING_NS_CC;
@@ -12,6 +11,7 @@ USING_NS_CC;
 class Character :public Sprite3D
 {
 public:
+	class Attribute;
 	Character();
 	~Character();
 	/* 增加/减少生命 */
@@ -27,7 +27,7 @@ public:
 	/* 获取当前得分 */
 	int getSorce() { return _sorce; }
 	/* 获取当前人物属性 */
-	//Attribute *getAttribute() { return _attribute; }
+	Attribute *getAttribute() { return _attribute; }
 	/* 攻击 */
 	void attack();
 	/* 死亡 */
@@ -37,10 +37,69 @@ private:
 	int _experience;		//当前已有经验
 	int _sorce;				//当前得分
 	WeaponType _weaponType;	//武器类型
-	//Attribute* _attribute;	//属性加成
+	Attribute* _attribute;	//属性加成
 	Vec3 _pos;				//人物移动后的坐标
 	bool _isDie;			//人物是否死亡
-	
 };
 
+
+/* 属性加成类 */
+class Character::Attribute
+{
+public:
+	Attribute();
+	~Attribute();
+	/* 创建一个属性对象 */
+	static Attribute* create();
+	/* 增加攻击力 */
+	void addAttackDamage(float add);
+	/* 增加射程 */
+	void addAttackRange(float add);
+	/* 增加攻击速度 */
+	void addAttackSpeed(float add);
+	/* 增加移动速度 */
+	void addMovingSpeed(float add);
+	/* 增加经验获取倍数 */
+	void addEmpiricalAcquisition(float add);
+	/* 增加防御力 */
+	void addDefensiveForce(float add);
+	/* 增加恢复能力 */
+	void addRestoringAbility(float add);
+	/* 临时属性：增加视野 */
+	void addView();
+	/* 临时属性：增加变小 */
+	void addSmall();
+	/* 临时属性：增加磁石 */
+	void addMagnet();
+	/* 临时属性：恢复视野 */
+	void delView();
+	/* 临时属性：恢复变大 */
+	void delSmall();
+	/* 临时属性：去除磁石 */
+	void delMagnet();
+	/* 更改临时能力持续时间 */
+	void setDuration(float add);
+private:
+	float _attackDamage;			//攻击力加成
+	float _attackRange;				//射程加成
+	float _attackSpeed;				//攻击速度加成
+	float _movingSpeed;				//移动速度加成
+	float _empiricalAcquisition;	//经验获取加成
+	float _defensiveForce;			//防御力加成
+	float _restoringAbility;		//恢复能力加成
+	/*
+	 * View 最低位
+	 * Small 中间位
+	 * Magnet 最高位
+	 */
+	enum TemporaryType
+	{
+		kTemporaryNone = 0x00,
+		kTemporaryView = 0x01,
+		kTemporarySmall = 0x02,
+		kTemporaryMagnet = 0x04
+	};
+	short int _temporary;			//临时属性类型
+	float _duration;				//临时能力持续时间
+};
 #endif
