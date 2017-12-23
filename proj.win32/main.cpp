42a907565dd6b39917ca10c20b2859728ac485e6
im0qianqian <qian1335661317@qq.com>
2017-12-23 18:13:51 +0800

    init

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -4,37 +4,15 @@
 
 USING_NS_CC;
 
-#if _MSC_VER > 1800
-#pragma comment(lib,"libcocos2d_2015.lib")
-#pragma comment(lib,"libbox2d_2015.lib")
-#pragma comment(lib,"libSpine_2015.lib")
-#pragma comment(lib,"librecast_2015.lib")
-#pragma comment(lib,"libbullet_2015.lib")
-#else
-#pragma comment(lib,"libcocos2d_2013.lib")
-#pragma comment(lib,"libbox2d_2013.lib")
-#pragma comment(lib,"libSpine_2013.lib")
-#pragma comment(lib,"librecast_2013.lib")
-#pragma comment(lib,"libbullet_2013.lib")
-#endif
-
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPTSTR    lpCmdLine,
-	int       nCmdShow)
+int WINAPI _tWinMain(HINSTANCE hInstance,
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	/*
-	输出重定向到控制台
-	*/
-	AllocConsole();
-	freopen("CONIN$", "r", stdin);
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
-	/* ********** */
-	// create the application instance
-	AppDelegate app;
-	return Application::getInstance()->run();
+    // create the application instance
+    AppDelegate app;
+    return Application::getInstance()->run();
 }
